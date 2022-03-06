69c8933ba52cb0f71cb7b52a53026b4c0ba75e69
angelgr-com <dev@angelgr.com>
2022-03-06 23:07:10 +0100

    Reorganize movies endpoints by GET and POST methods. Add actors and genres methods

--- controllers/MoviesController.js
+++ controllers/MoviesController.js
@@ -3,70 +3,100 @@ const TMDB = require('../config/TMDB');
 const { default: axios } = require('axios');
 const { Movie } = require('../models/index');
 const { Genre } = require('../models/index');
-let page = 1;
 
-// Every time saves one different page (20 movies) from TMDB's top rated movies
-MoviesController.saveTopRatedMovies = async (req, res) => {
-    // https://developers.themoviedb.org/3/movies/get-top-rated-movies
+MoviesController.getAllMovies = (req, res) => {
+    Movie.findAll()
+    .then(movies => {
+        if(movies != 0){
+            res.send(movies);
+        }else {
+            res.send(`There are no movies in the database.`);
+        }
+    }).catch(error => {
+        res.send(error);
+    });
+}
+
+MoviesController.getActors = async (req, res) => {
+    let movie_id = req.params.movie_id;
+    // https://developers.themoviedb.org/3/movies/get-movie-credits
     let results = await axios.get(`
-    https://api.themoviedb.org/3/movie/top_rated?api_key=${TMDB.api_key}&language=${TMDB.language}&page=${page}`);
-    page++;
-    
-    let array = [];
-    let number = 0;
-    for (let i=0;i<20;i++){
-        let tmdbID = results.data.results[i].id;
-        // https://developers.themoviedb.org/3/movies/get-movie-external-ids
-        let externalIDs = await axios.get(`https://api.themoviedb.org/3/movie/${tmdbID}/external_ids?api_key=${TMDB.api_key}&`);
-
-        Movie.findOne({
-            where : { tmdb_id: tmdbID }
-        }).then(movie => {
-            number++;
-            if(movie === null) {
-                Movie.create({
-                    title: results.data.results[i].title,
-                    tmdb_id: tmdbID,
-                    imdb_id: externalIDs.data.imdb_id,
-                    facebook_id: externalIDs.data.facebook_id,
-                    instagram_id: externalIDs.data.instagram_id,
-                    twitter_id: externalIDs.data.twitter_id,
-                    popularity: results.data.results[i].popularity,
-                    poster_path: results.data.results[i].poster_path,
-                    release_date: results.data.results[i].release_date,
-                    video: results.data.results[i].video,
-                    vote_average: results.data.results[i].vote_average,
-                    vote_count: results.data.results[i].vote_count,
-                });
-            }
-        }).catch(error => {
-            res.send(error);
-        });
+        https://api.themoviedb.org/3/movie/${movie_id}/credits?api_key=${TMDB.api_key}&language=${TMDB.language}
+    `);
+    let actors = results.data.cast;   
+    for (let i=actors.length-1; i>=0; i--) {
+        if (actors[i].popularity < 10.0) {
+            actors.splice(i,1);
+        }
     }
-    if(number > 0)
-        res.send(`${number+1} top rated movies were added.`);
-    else
-        res.send('No movies were added');
+
+    res.send(actors);
+}
+
+MoviesController.search = async (req, res) => {
+    let id = req.query.id;
+    let title = req.query.title;
+    try {
+        if(req.query.id) {
+            if(isNaN(parseInt(req.query.id))) {
+                res.send('Incorrect ID. Check your TMDB ID number.');
+            } else {
+                // https://developers.themoviedb.org/3/movies/get-movie-details
+                let results = await axios.get(`
+                https://api.themoviedb.org/3/movie/${id}?api_key=${TMDB.api_key}&language=${TMDB.language}
+                `);
+                res.send(results.data);
+            }
+        } else if (req.query.title) {
+            // https://developers.themoviedb.org/3/search/search-movies
+            let results = await axios.get(`
+            https://api.themoviedb.org/3/search/movie?api_key=${TMDB.api_key}&query=${title}&language=${TMDB.language}&page=1&include_adult=false
+            `);
+            res.send(results.data);
+        } else {
+            res.send('Incorrect query. Please, search by title or by TMDB ID number.');
+        }
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+MoviesController.getGenres = async (req, res) => {
+    Genre.findAll()
+    .then(() => {
+        Genre.findAll()
+        .then(genres => {
+            if(genres != 0){
+                res.send(genres);
+            }else {
+                // res.send(`There are no movies in the database.`);
+            }
+        })
+    })
+    .catch(error => {
+        res.send(error);
+    });
 }
 
 MoviesController.saveMovieByID = async (req, res) => {
     // https://developers.themoviedb.org/3/movies/get-movie-details
     let results = await axios.get(`
-    https://api.themoviedb.org/3/movie/${req.params.id}?api_key=${TMDB.api_key}&language=${TMDB.language}`);
+    https://api.themoviedb.org/3/movie/${req.params.movie_id}?api_key=${TMDB.api_key}&language=${TMDB.language}`);
     
     // https://developers.themoviedb.org/3/movies/get-movie-external-ids
     let externalIDs = await axios.get(`
-    https://api.themoviedb.org/3/movie/${req.params.id}/external_ids?api_key=${TMDB.api_key}&`);
+    https://api.themoviedb.org/3/movie/${req.params.movie_id}/external_ids?api_key=${TMDB.api_key}&`);
+
 
     Movie.findOne({
-        where : { tmdb_id: req.params.id }
+        where : { tmdb_id: req.params.movie_id }
     })
     .then(movie => {
         if(movie != null) {
             res.send('The movie is already in the database');
         } else {
             Movie.create({
-                tmdb_id: req.params.id,
+                tmdb_id: req.params.movie_id,
                 imdb_id: externalIDs.data.imdb_id,
                 facebook_id: externalIDs.data.facebook_id,
                 instagram_id: externalIDs.data.instagram_id,
@@ -91,44 +121,50 @@ MoviesController.saveMovieByID = async (req, res) => {
     });
 }
 
-MoviesController.search = async (req, res) => {
-    let id = req.query.id;
-    let title = req.query.title;
-    try {
-        if(req.query.id) {
-            // https://developers.themoviedb.org/3/movies/get-movie-details
-            let results = await axios.get(`
-            https://api.themoviedb.org/3/movie/${id}?api_key=${TMDB.api_key}&language=${TMDB.language}
-            `);
-            res.send(results.data);
-        } else if (req.query.title) {
-            // https://developers.themoviedb.org/3/search/search-movies
-            let results = await axios.get(`
-            https://api.themoviedb.org/3/search/movie?api_key=${TMDB.api_key}&query=${title}&language=${TMDB.language}&page=1&include_adult=false
-            `);
-            res.send(results.data);
-        } else {
-            res.send('Incorrect query. Please, search by title or by TMDB movie id.');
-        }
-    } catch (error) {
-        console.log(error);
+// Every time saves one different page (20 movies) from TMDB's top rated movies
+MoviesController.saveTopRatedMovies = async (req, res) => {
+    let page = Math.floor(Math.random() * (10 - 1 + 1) + 1);
+    // https://developers.themoviedb.org/3/movies/get-top-rated-movies
+    let results = await axios.get(`
+    https://api.themoviedb.org/3/movie/top_rated?api_key=${TMDB.api_key}&language=${TMDB.language}&page=${page}`);
+    let movies = 0;
+    for (let i=0;i<20;i++) {
+        let tmdbID = results.data.results[i].id;
+        // https://developers.themoviedb.org/3/movies/get-movie-external-ids
+        let externalIDs = await axios.get(`https://api.themoviedb.org/3/movie/${tmdbID}/external_ids?api_key=${TMDB.api_key}&`);
+
+        // Only saves to the database if movie is not added
+        Movie.findOne({
+            where : { tmdb_id: tmdbID }
+        }).then(movie => {
+            if(movie === null) {
+                movies++;
+                Movie.create({
+                    title: results.data.results[i].title,
+                    tmdb_id: tmdbID,
+                    imdb_id: externalIDs.data.imdb_id,
+                    facebook_id: externalIDs.data.facebook_id,
+                    instagram_id: externalIDs.data.instagram_id,
+                    twitter_id: externalIDs.data.twitter_id,
+                    popularity: results.data.results[i].popularity,
+                    poster_path: results.data.results[i].poster_path,
+                    release_date: results.data.results[i].release_date,
+                    video: results.data.results[i].video,
+                    vote_average: results.data.results[i].vote_average,
+                    vote_count: results.data.results[i].vote_count,
+                });
+            }
+        }).catch(error => {
+            res.send(error);
+        });
     }
+    movies > 0 ?
+        res.send(`${movies+1} movies from TMDB's Top Rated Movies page ${page} were added.`)
+    :
+        res.send(`No movies from TMDB's Top Rated Movies page ${page} were added.`);
 }
 
-MoviesController.getAllMovies = (req, res) => {
-    Movie.findAll()
-    .then(movies => {
-        if(movies != 0){
-            res.send(movies);
-        }else {
-            res.send(`There are no movies in the database.`);
-        }
-    }).catch(error => {
-        res.send(error);
-    });
-}
-
-MoviesController.getGenres = async (req, res) => {
+MoviesController.saveGenres = async (req, res) => {
     let results = await axios.get(`https://api.themoviedb.org/3/genre/movie/list?api_key=${TMDB.api_key}&language=${TMDB.language}`);
 
     const saveGenres = () => {
@@ -142,27 +178,13 @@ MoviesController.getGenres = async (req, res) => {
             });
         }
     };
-    
+
+    // Only saves in the database if Genres table is empty
     Genre.findOne({
         where : { id_tmdb : results.data.genres[0].id }
     })
     .then(genre => {
-        if (genre != null) {
-            Genre.findAll()
-            .then(() => {
-                Genre.findAll()
-                .then(genres => {
-                    if(genres != 0){
-                        res.send(genres);
-                    }else {
-                        // res.send(`There are no movies in the database.`);
-                    }
-                })
-            })
-            .catch(error => {
-                res.send(error);
-            });
-        } else {
+        if (genre == null) {
             saveGenres();
             res.send(`Genres added to the database.`)
         }
--- views/MoviesRouter.js
+++ views/MoviesRouter.js
@@ -2,19 +2,23 @@ const express = require('express');
 const router = express.Router();
 const MoviesController = require('../controllers/MoviesController');
 
+// GET ENDPOINTS
+// http://localhost:5000/movies/
 router.get('/', MoviesController.getAllMovies);
-
-// http://localhost:5000/movies/add/
-router.get('/add', MoviesController.saveTopRatedMovies);
-
-// http://localhost:5000/movies/add/619264
-router.get('/add/:id', MoviesController.saveMovieByID);
-
 // http://localhost:5000/movies/search?id="619264"&title="the king's man"
 router.get('/search', MoviesController.search);
-
+// http://localhost:5000/movies/genres/
 router.get('/genres', MoviesController.getGenres);
-// router.get('/actors', MoviesController.getActors);
+// http://localhost:5000/movies/actors/
+router.get('/actors/:movie_id', MoviesController.getActors);
+
+// POST ENDPOINTS
+// http://localhost:5000/movies/add/619264
+router.post('/add/:movie_id', MoviesController.saveMovieByID);
+// http://localhost:5000/movies/add/
+router.post('/add', MoviesController.saveTopRatedMovies);
+// http://localhost:5000/movies/genres/
+router.post('/genres', MoviesController.saveGenres);
 
 // router.get('/filter/genre', MoviesController.filterByGenre);
 // router.get('/filter/actors', MoviesController.filterByActors);
